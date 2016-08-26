97b6ef4a1c46e15023544a6b12e93b174890c999
manonat <buddhacomic@hotmail.com>
2016-08-26 14:37:46 +0700

    Register success เช็คชือซ้ำแล้ว

--- app/src/main/java/com/virtualsiamu/rms/Register.java
+++ app/src/main/java/com/virtualsiamu/rms/Register.java
@@ -172,10 +172,42 @@ public class Register extends AppCompatActivity {
                     "รหัสบุคลากรผิด", "ไม่มีรหัสนี้ในฐานข้อมูล กรุณากรอก รหัสบุคลากรใหม่");
 
 
+        } else if (checkUser(userString)) {
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, R.drawable.doremon48,
+                    "User ซ้ำ", "กรุณากรอก User ใหม่ User ซ้ำ");
+
+
         } else {
-            Log.d("26AugV3", "รหัสบุคลากรถูกแล้ว");
+            confirmUpload();
         }
+
+
     }   // clickSign
+
+    private boolean checkUser(String userString) {
+
+        // true  ==> user duplicate
+        // false ==> userOk
+        boolean bolResult = false;
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM identityTABLE", null);
+        cursor.moveToFirst();
+        for (int i=0;i<cursor.getCount();i++) {
+
+            if (userString.equals(cursor.getString(cursor.getColumnIndex(MyManage.column_UserID)))) {
+                bolResult = true;
+            }
+
+            cursor.moveToNext();
+        } //for
+        cursor.close();
+
+        return bolResult;
+    }
 
     private boolean checkNameID(String nameIDString) {
 
@@ -200,6 +232,7 @@ public class Register extends AppCompatActivity {
             }//if
 
             cursor.moveToNext();
+            return bolResult;
         }//for
 
 
